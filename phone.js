cebbbdbe01343746de425b3fd4d75aba1041db57
Arya-Jain <89778433+Arya-Jain@users.noreply.github.com>
2022-02-06 11:05:14 +0530

    Add files via upload

---
+++ phone.js
@@ -0,0 +1,48 @@
+img = "";
+status = "";
+objects= [];
+
+function preload(){
+    img = loadImage("phone.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status - Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("model loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+
+    if(status != ""){
+
+        for(i= 0; i< objects.length; i++ ){
+        document.getElementById("status").innerHTML = "I have tested 1 object and Coco ssd has detected 1 from it ";
+        fill("red");
+        percent = floor(objects[i].confidence*100);
+
+        text(objects[i].label ,  objects[i].x+20 , objects[i].y+20);
+        noFill();
+        stroke("red");
+        rect( objects[i].x, objects[i].y , objects[i].width, objects[i].height );
+
+        }
+    }
+}
